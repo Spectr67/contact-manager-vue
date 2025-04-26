4c116496af6082cc92af386d37e2e1a5f7adebdf
Arthur <pennywennyweed@gmail.com>
2025-04-26 23:02:51 +0300

    second init

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
-<script
-  src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"
->
+<script>
+// src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"
 
-import initPage from './main'
+import something from './main'
+document.addEventListener('DOMContentLoaded', function () {
+  const elemsA = document.querySelectorAll('a')
+  const elModal3 = document.querySelector('#modal3')
+
+  elemsA.forEach(elA => elA.setAttribute('draggable', false))
+
+  M.AutoInit()
+
+  document.querySelector('#search ~ i').addEventListener('click', function () {
+    M.Modal.getInstance(elModal3).close()
+    document.querySelector('#search').value = ''
+  })
+
+  document.querySelector('#search').addEventListener('click', function () {
+    M.Modal.prototype._handleFocus = () => {}
+    M.Modal.getInstance(elModal3).open()
+    this.focus()
+  })
+})
 </script>
 <template>
   <head>
--- src/main.js
+++ src/main.js
@@ -5,26 +5,22 @@ import './style.css'
 
 createApp(App).mount('#app')
 
-export default function initPage() {
-  document.addEventListener('DOMContentLoaded', function () {
-    const elemsA = document.querySelectorAll('a')
-    const elModal3 = document.querySelector('#modal3')
+export default document.addEventListener('DOMContentLoaded', function () {
+  const elemsA = document.querySelectorAll('a')
+  const elModal3 = document.querySelector('#modal3')
 
-    elemsA.forEach(elA => elA.setAttribute('draggable', false))
+  elemsA.forEach(elA => elA.setAttribute('draggable', false))
 
-    M.AutoInit()
+  M.AutoInit()
 
-    document
-      .querySelector('#search ~ i')
-      .addEventListener('click', function () {
-        M.Modal.getInstance(elModal3).close()
-        document.querySelector('#search').value = ''
-      })
-
-    document.querySelector('#search').addEventListener('click', function () {
-      M.Modal.prototype._handleFocus = () => {}
-      M.Modal.getInstance(elModal3).open()
-      this.focus()
-    })
+  document.querySelector('#search ~ i').addEventListener('click', function () {
+    M.Modal.getInstance(elModal3).close()
+    document.querySelector('#search').value = ''
   })
-}
+
+  document.querySelector('#search').addEventListener('click', function () {
+    M.Modal.prototype._handleFocus = () => {}
+    M.Modal.getInstance(elModal3).open()
+    this.focus()
+  })
+})
