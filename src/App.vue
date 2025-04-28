5f30440fe5d0b7f2923e8e521a523509ed7fdfcb
Arthur <pennywennyweed@gmail.com>
2025-04-29 01:03:26 +0300

    sliced ui navigation

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import RecentCallsList from './components/RecentCalls/RecentCallsList.vue'
 import FavoritesList from './components/Favorites/FavoritesList.vue'
 import SearcherResult from './components/Search/SearcherResult.vue'
 import ContactPreview from './components/ContactPreview.vue'
+import UINavagation from './components/ui/UINavagation.vue'
 
 export default {
   components: {
@@ -18,6 +19,7 @@ export default {
     FavoritesList,
     SearcherResult,
     ContactPreview,
+    UINavagation,
   },
 }
 </script>
@@ -28,25 +30,7 @@ export default {
       <Searcher />
 
       <!-- кнопки выбора меню -->
-      <div class="nav-content">
-        <ul class="tabs tabs-transparent">
-          <li class="tab">
-            <a href="#tab-1">
-              <span class="material-symbols-outlined">star</span>
-            </a>
-          </li>
-          <li class="tab">
-            <a href="#tab-2">
-              <span class="material-symbols-outlined">chronic</span>
-            </a>
-          </li>
-          <li class="tab">
-            <a class="active" href="#tab-3">
-              <span class="material-symbols-outlined">people</span>
-            </a>
-          </li>
-        </ul>
-      </div>
+      <UINavagation />
     </nav>
 
     <div>
